c124d81f1fd07597e2aeaf2be120fb4cda7e8b20
elpwc/wniko <elpwc@hotmail.com>
2025-01-07 23:21:04 +0900

    fix: build

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,14 @@ function App() {
       <Routes>
         <Route path="" element={<Main />}>
           <Route index element={<MapPage game={Game.maimaidx} version={GameVersion.ja} />}></Route>
-          <Route path="mamadx">
-            <Route path="" element={<MapPage game={Game.maimaidx} version={GameVersion.ja} />}></Route>
-            <Route path="ja" element={<MapPage game={Game.maimaidx} version={GameVersion.ja} />}></Route>
-            <Route path="inter" element={<MapPage game={Game.maimaidx} version={GameVersion.inter} />}></Route>
-          </Route>
-          <Route path="chuni">
-            <Route path="" element={<MapPage game={Game.chuni} version={GameVersion.ja} />}></Route>
-            <Route path="ja" element={<MapPage game={Game.chuni} version={GameVersion.ja} />}></Route>
-            <Route path="inter" element={<MapPage game={Game.chuni} version={GameVersion.ja} />}></Route>
-          </Route>
+          <Route path="mamadx"></Route>
+          <Route path="mamadx/ja" element={<MapPage game={Game.maimaidx} version={GameVersion.ja} />}></Route>
+          <Route path="mamadx/inter" element={<MapPage game={Game.maimaidx} version={GameVersion.inter} />}></Route>
+          
+          <Route path="chuni"></Route>
+          <Route path="chuni/ja" element={<MapPage game={Game.chuni} version={GameVersion.ja} />}></Route>
+          <Route path="chuni/inter" element={<MapPage game={Game.chuni} version={GameVersion.ja} />}></Route>
+          
           <Route path="ongk" element={<MapPage game={Game.ongeki} />}></Route>
 
           <Route path="about" element={<About />}></Route>
